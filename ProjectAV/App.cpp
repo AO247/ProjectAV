a6b54162293a452ca89e346be58dba0b5e92fa3b
Alamek <162360463+AO247@users.noreply.github.com>
2025-03-24 21:06:29 +0100

    mainLoop Ep.10

---
+++ ProjectAV/App.cpp
@@ -0,0 +1,27 @@
+#include "App.h"
+
+App::App()
+	:
+	wnd(800, 600, "ProjectAV")
+{
+}
+
+int App::Go()
+{
+	while (true)
+	{
+		if (const auto ecode = Window::ProcessMessages())
+		{
+			return *ecode;
+		}
+		DoFrame();
+	}
+}
+
+void App::DoFrame()
+{
+	const float t = timer.Peek();
+	std::ostringstream oss;
+	oss << "Time elapsed: " << std::setprecision(1) << std::fixed << t << "s"; 
+	wnd.SetTitle(oss.str());
+}
--- ProjectAV/WinMain.cpp
+++ ProjectAV/WinMain.cpp
@@ -1,25 +1,10 @@
 #include "Window.h"
-
+#include "App.h"
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	try
 	{
-	Window wnd(800, 600, "Avatar");
-
-	MSG msg;
-	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	if (gResult == -1)
-	{
-		return -1;
-	}
-	else {
-		return msg.wParam;
-	}
+		return App{}.Go();
 	}
 	catch (const CException& e)
 	{
