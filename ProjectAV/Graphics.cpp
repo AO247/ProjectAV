f185a95f367f00f7eb525fe29d9809ba95a4797e
Alamek <162360463+AO247@users.noreply.github.com>
2025-03-25 16:02:34 +0100

    rotating Ep.18

--- ProjectAV/Graphics.cpp
+++ ProjectAV/Graphics.cpp
@@ -98,7 +98,7 @@ void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
 	pContext->ClearRenderTargetView( pTarget.Get(),color );
 }
 
-void Graphics::DrawTestTriangle()
+void Graphics::DrawTestTriangle(float angle)
 {
 	namespace wrl = Microsoft::WRL;
 	HRESULT hr;
@@ -178,8 +178,40 @@ void Graphics::DrawTestTriangle()
 	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 
-	wrl::ComPtr<ID3DBlob> pBlob;
+	// Create constant buffer for transformation matrix
+	struct ConstantBuffer
+	{
+		struct
+		{
+			float element[4][4];
+		} transformation;
+	};
 
+	const ConstantBuffer cb =
+	{
+		{
+			(3.0f/4.0f) * std::cos(angle),		std::sin(angle),	0.0f, 0.0f,
+			(3.0f / 4.0f) * -std::sin(angle),	std::cos(angle),	0.0f, 0.0f,
+			0.0f,								0.0f,				1.0f, 0.0f,
+			0.0f,								0.0f,				0.0f, 1.0f
+		}
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd = {};
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof(cb);
+	cbd.StructureByteStride = 0u;
+
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&cbd, &csd, &pConstantBuffer));
+	pContext->VSSetConstantBuffers(0u, 1u, pConstantBuffer.GetAddressOf());
+
+	wrl::ComPtr<ID3DBlob> pBlob;
 	//Pixel Shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
 	GFX_THROW_INFO(D3DReadFileToBlob(L"PixelShader.cso", &pBlob));
