28f3ddedbb6732a22ea87a8e9efc1758a5384f37
Sajdens <filipus413@gmail.com>
2025-05-25 23:59:39 +0200

    Poprawa legendarnego błędu Adama z audio

--- ProjectAV/SoundDevice.cpp
+++ ProjectAV/SoundDevice.cpp
@@ -64,7 +64,7 @@ void SoundDevice::SetAttenuation(int key)
 	if (key < 0xD001 || key > 0xD006)
 		throw ("Invalid key for attenuation");
 
-	alListeneri(AL_DISTANCE_MODEL, key);
+	alDistanceModel(key);
 }
 
 /// <summary>
