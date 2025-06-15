30c8b8779c5b79ef4d91fc7721afdba156491682
Kamil Marel <marel.kamilg@gmail.com>
2025-06-15 14:38:42 +0200

    skinned characters are now shaded

--- ProjectAV/Skinned_VS.hlsl
+++ ProjectAV/Skinned_VS.hlsl
@@ -17,7 +17,7 @@ struct VS_INPUT
 struct PS_INPUT
 {
     float4 clipPosition : SV_Position;
-    //float3 viewNormal : NORMAL;
+    float3 viewNormal : NORMAL;
     float2 texCoord : TEXCOORD0;
     float3 viewPosition : TEXCOORD1;
 };
@@ -27,13 +27,10 @@ PS_INPUT main(VS_INPUT input)
     PS_INPUT output;
     
     float4 totalPosition = float4(0.0f, 0.0f, 0.0f, 0.0f);
-    //totalPosition = float4(input.position, 1.0f);
-    
-    //float4 totalNormal = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 totalNormal = float4(0.0f, 0.0f, 0.0f, 0.0f);
     
     // ZAKLADAM, ZE MAX 4 KOSCI MAJA WPLYW
     // ORAZ MOZEMY MIEC MAX 100 KOSCI
-    
     
     for (int i = 0; i < 4; i++)
     {
@@ -49,18 +46,13 @@ PS_INPUT main(VS_INPUT input)
 
         float4 localPosition = mul(float4(input.position, 1.0f), finalBoneMatrices[input.boneIDs[i]]);
         totalPosition += localPosition * input.boneWeights[i];
-        //float3 localNormal = mul((float3x3) finalBoneMatrices[input.boneIDs[i]], input.normal);
-        //totalNormal += localNormal * input.boneWeights[i];
+        float4 localNormal = mul(float4(input.normal, 0.0f), finalBoneMatrices[input.boneIDs[i]]);
+        totalNormal += localNormal * input.boneWeights[i];
     }
-    
-    
-    //output.clipPosition = mul(modelViewProj, totalPosition);
-    //output.viewPosition = mul(modelView, totalPosition).xyz;
+
     output.clipPosition = mul(totalPosition, modelViewProj);
     output.viewPosition = mul(totalPosition, modelView).xyz;
-    //kom -----
-    //output.viewNormal = normalize(mul(skinnedNormal.xyz, (float3x3) modelView));
-    //kom -----
+    output.viewNormal = normalize(mul(totalNormal.xyz, (float3x3) modelView));
     output.texCoord = input.texCoord;
     
     return output;
