081ce7b41ed15b2febe6709afdacba3b467f2306
Zuzanna Lewandowska <miniaturkazdl@gmail.com>
2025-06-24 16:01:04 +0200

    poprawione cienie

--- ProjectAV/ShadowSkinned_VS.hlsl
+++ ProjectAV/ShadowSkinned_VS.hlsl
@@ -2,7 +2,7 @@
 
 // Bufor sta³y z macierz¹ transformacji œwiat³a
 // Musi pasowaæ do tego, co ustawia twoja klasa ShadowCbuf
-cbuffer ShadowTransform : register(b0)
+cbuffer ShadowCbuf : register(b0)
 {
     matrix lightTransform; // Zak³adam, ¿e to jest World * LightViewProj
 };
@@ -33,20 +33,15 @@ VS_OUTPUT main(VS_INPUT input)
 {
     float4 totalLocalPos = float4(0.0f, 0.0f, 0.0f, 0.0f);
     
-    // --- Logika Skinningu (skopiowana i uproszczona z Skinned_VS.hlsl) ---
-    // Pêtla po 4 koœciach wp³ywaj¹cych na wierzcho³ek
+    // --- Logika Skinningu ---
     for (int i = 0; i < 4; i++)
     {
-        // Sprawdzamy, czy waga jest wiêksza od zera, aby unikn¹æ zbêdnych obliczeñ
         if (input.boneWeights[i] > 0.0f)
         {
-            // Transformujemy pozycjê wierzcho³ka przez macierz koœci i mno¿ymy przez wagê
             totalLocalPos += mul(float4(input.position, 1.0f), finalBoneMatrices[input.boneIDs[i]]) * input.boneWeights[i];
         }
     }
 
-    // Jeœli wagi nie sumuj¹ siê do 1 (lub nie ma wag), u¿yj oryginalnej pozycji
-    // To zabezpieczenie przed "znikaj¹cymi" wierzcho³kami
     if (totalLocalPos.w == 0.0f)
     {
         totalLocalPos = float4(input.position, 1.0f);
@@ -54,7 +49,6 @@ VS_OUTPUT main(VS_INPUT input)
     
     // --- Transformacja do przestrzeni œwiat³a ---
     VS_OUTPUT output;
-    // Mno¿ymy zdeformowan¹ pozycjê przez macierz transformacji œwiat³a
     output.clipPosition = mul(totalLocalPos, lightTransform);
     
     return output;
